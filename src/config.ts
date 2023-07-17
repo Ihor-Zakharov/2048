c49a272d16490574bdd37f8f8066b94f15f31dde
Ihor Zakharov <ihor.o.zakharov@outlook.com>
2023-07-17 20:03:56 +0300

    Swipe distance refinements.

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ export const config = {
   originalGameUrl: "https://play2048.co/",
   phoneScaleFactorPixelData: 0.62,
   repoUrl: "https://github.com/",
-  swipeDistance: 100
+  swipeDistance: 40
 } as const;
 
 export type ConfigType = typeof config;
