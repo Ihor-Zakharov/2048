c42f97e249727c9cc158118f9d867bc9884fea13
Ihor Zakharov <ihor.o.zakharov@outlook.com>
2023-07-22 16:59:55 +0300

    Remove todos.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,3 @@ import { Game } from "@components/game/game";
 import "styles/main.scss";
 
 window.onload = () => new Game();
-
-// TODO: implement favicon as svg, convert to ico
-// TODO: add meta tags to index.html
